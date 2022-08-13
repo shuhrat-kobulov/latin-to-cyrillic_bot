59b6c181a41fd0e1871d1301001619a9dd424cb5
mrshukhratbek <shukhratbekqobulov@gmail.com>
2022-08-13 18:13:45 +0500

    feat: add functionality

--- index.py
+++ index.py
@@ -1,6 +1,23 @@
-from operator import imod
-
-
 import telebot
 
-bot = telebot.TeleBot('', parse_mode=None);
+from translate import to_cyrillic, to_latin
+from config import BOT_TOKEN
+
+bot = telebot.TeleBot(BOT_TOKEN, parse_mode=None);
+
+@bot.message_handler(commands=['start'])
+def send_welcome(message):
+  bot.reply_to(message, "Hello! Welcome to @Latin2Cyrilic_Bot")
+
+@bot.message_handler(func=lambda m: True)
+def echo_all(message):
+  msg = message.text
+
+  if msg.isascii():
+    response = to_cyrillic(msg)
+  else:
+    response = to_latin(msg)
+
+  bot.reply_to(message, response)
+
+bot.polling()
